b8e15c284336c7eee5cb8e572902ac32c7ab553a
D.A Han <dawun.han@gmail.com>
2020-01-07 21:16:39 +1100

    Middleware for private and public

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import passport from 'passport';
+import mongoose from 'mongoose';
 import session from 'express-session';
+import MongoStore from 'connect-mongo';
 import {localsMiddleware} from './middlewares';
 import routes from './routes';
 import userRouter from './routers/userRouter';
@@ -13,6 +15,8 @@ import globalRouter from './routers/globalRouter';
 
 import './passport';
 const app = express ();
+
+const CookieStore = MongoStore (session);
 
 app.use (helmet ());
 app.set ('view engine', 'pug');
@@ -27,6 +31,9 @@ app.use (
     secret: process.env.COOKIE_SECRET,
     resave: true,
     saveUninitialized: false,
+    store: new CookieStore ({
+      mongooseConnection: mongoose.connection,
+    }),
   })
 );
 app.use (passport.initialize ());
