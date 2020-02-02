141b2e7d8c7068752bef4dfb24b5f7fbc078aca1
D.A Han <dawun.han@gmail.com>
2020-02-03 00:12:34 +1100

    Connect to AWS S3

--- middlewares.js
+++ middlewares.js
@@ -1,30 +1,51 @@
 import multer from 'multer';
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
 import routes from './routes';
 
-const multerVideo = multer ({dest: 'uploads/videos/'});
-const multerAvatar = multer ({dest: 'uploads/avatars/'});
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_PRIVATE_KEY,
+  region: 'ap-southeast-2',
+});
+
+const multerVideo = multer({
+  storage: multerS3({
+    s3,
+    acl: 'public-read',
+    bucket: 'itubesite/video',
+  }),
+});
+const multerAvatar = multer({
+  storage: multerS3({
+    s3,
+    acl: 'public-read',
+    bucket: 'itubesite/avatar',
+  }),
+});
+
+export const uploadVideo = multerVideo.single('videoFile');
+export const uploadAvatar = multerAvatar.single('avatar');
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = 'iTube';
   res.locals.routes = routes;
   res.locals.loggedUser = req.user || null;
-  next ();
+  next();
 };
 
 export const onlyPublic = (req, res, next) => {
   if (req.user) {
-    res.redirect (routes.home);
+    res.redirect(routes.home);
   } else {
-    next ();
+    next();
   }
 };
 
 export const onlyPrivate = (req, res, next) => {
   if (req.user) {
-    next ();
+    next();
   } else {
-    res.redirect (routes.home);
+    res.redirect(routes.home);
   }
 };
-export const uploadVideo = multerVideo.single ('videoFile');
-export const uploadAvatar = multerAvatar.single ('avatar');
