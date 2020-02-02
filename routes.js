c57d08b2342785a912bc046fc768ada96ffdb50d
D.A Han <dawun.han@gmail.com>
2020-02-02 23:20:02 +1100

    Comment

--- routes.js
+++ routes.js
@@ -30,6 +30,7 @@ const FB_CALLBACK = '/auth/facebook/callback';
 // API
 const API = '/api';
 const REGISTER_VIEW = '/:id/view';
+const ADD_COMMENT = '/:id/comment';
 
 const routes = {
   home: HOME,
@@ -77,6 +78,7 @@ const routes = {
   facebookCallback: FB_CALLBACK,
   api: API,
   registerView: REGISTER_VIEW,
+  addComment: ADD_COMMENT,
 };
 
 export default routes;
