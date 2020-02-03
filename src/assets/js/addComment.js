b99c734cb446f9acb176b942288c06ed3918d505
D.A Han <dawun.han@gmail.com>
2020-02-03 23:08:48 +1100

    addComment fix

--- src/assets/js/addComment.js
+++ src/assets/js/addComment.js
@@ -1,46 +1,65 @@
-import axios from "axios";
+import axios from 'axios';
 
-const addCommentForm = document.getElementById("jsAddComment");
-const commentList = document.getElementById("jsCommentList");
-const commentNumber = document.getElementById("jsCommentNumber");
+const addCommentForm = document.getElementById('jsAddComment');
+const commentList = document.getElementById('jsCommentList');
+const commentNumber = document.getElementById('jsCommentNumber');
 
 const increaseNumber = () => {
   commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1;
 };
 
 const addComment = comment => {
-  const li = document.createElement("li");
-  const span = document.createElement("span");
+  const li = document.createElement('li');
+  const span = document.createElement('span');
   span.innerHTML = comment;
   li.appendChild(span);
   commentList.prepend(li);
   increaseNumber();
 };
 
+// const sendComment = async comment => {
+//   const videoId = window.location.href.split("/videos/")[1];
+//   const response = await axios({
+//     url: `/api/${videoId}/comment`,
+//     method: "POST",
+//     data: {
+//       comment
+//     }
+//   });
+//   if (response.status === 200) {
+//     addComment(comment);
+//   }
+// };
+
 const sendComment = async comment => {
-  const videoId = window.location.href.split("/videos/")[1];
+  const videoId = window.location.href.split('/videos/')[1];
   const response = await axios({
     url: `/api/${videoId}/comment`,
-    method: "POST",
+    method: 'POST',
     data: {
-      comment
-    }
+      comment,
+    },
   });
-  if (response.status === 200) {
-    addComment(comment);
+
+  try {
+    if (response.status === 200) {
+      addComment(comment);
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
 
 const handleSubmit = event => {
   event.preventDefault();
-  const commentInput = addCommentForm.querySelector("input");
+  const commentInput = addCommentForm.querySelector('input');
   const comment = commentInput.value;
   sendComment(comment);
-  commentInput.value = "";
+  commentInput.value = '';
 };
 
 function init() {
-  addCommentForm.addEventListener("submit", handleSubmit);
+  addCommentForm.addEventListener('submit', handleSubmit);
 }
 
 if (addCommentForm) {
