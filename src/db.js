689366bc0f164cc75007ddd3bac00a64748961eb
D.A Han <dawun.han@gmail.com>
2020-02-03 21:45:29 +1100

    db fix

--- src/db.js
+++ src/db.js
@@ -2,16 +2,19 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL_PROD, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 
 const handleOpen = () => console.log('✅ Connected to DB');
-const handleError = () => console.log(`❌ Error on DB connection: ${error}`);
+//const handleError = () => console.log(`❌ Error on DB connection: ${error}`);
 
 db.once('open', handleOpen);
-db.on('error', handleError);
+//db.on('error', handleError);
